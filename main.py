a351dae02dd976a0520141ccef259af5f6bd72ea
Archie <archiees@icloud.com>
2024-11-24 21:51:01 +0000

    clean up code

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import turtle
 
+def getXLines(x, y, cubewidth):
+    xlines = []
+    i = -(x/2)
+
+    while i <= x/2:
+        xlines.append(int(i))
+        i += cubewidth
+    return xlines
+
+def getYLines(x, y, cubewidth):
+    ylines =[]
+    i = (y/2)
+
+    while i >= -(y/2):
+        ylines.append(int(i))
+        i -= cubewidth
+
+    return ylines
+
 def drawGrid(t, x, y):
     CUBEWIDTH = 100
     
     t.penup()
     t.goto(-(x/2), (y/2))
 
-    xlines = []
+    xlines = getXLines(x, y, CUBEWIDTH)
+    ylines = getYLines(x, y, CUBEWIDTH)
 
-    i = -(x/2)
-    
-    while i <= x/2:
-        xlines.append(int(i))
-        i += CUBEWIDTH
 
+    # draw each line on y axis
     t.right(90)
-    
+
     for i in xlines:
         t.goto(i, (y/2))
 
@@ -23,16 +39,7 @@ def drawGrid(t, x, y):
         t.forward(y)
         t.penup()
 
-    ylines = []
-
-    i = (y/2)
-    
-    while i >= -(y/2):
-        ylines.append(int(i))
-        i -= CUBEWIDTH
-
-    print(ylines)
-    
+    # draw each line on y axis
     t.left(90)
     
     for i in ylines:
@@ -48,7 +55,7 @@ def main():
     screen.setup(500 + 4, 500 + 8)
     
     john = turtle.Turtle()
-    john.speed(696996)
+    john.speed(696969)
 
     drawGrid(john, 500, 500)
 
